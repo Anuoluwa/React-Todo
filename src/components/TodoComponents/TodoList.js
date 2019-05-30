89adbd139c13d7be8dc3121059a15e28d52877a3
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-05-30 15:19:37 +0100

    completes  mvp day 2

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,13 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
 import React from 'react';
+import Todo from './Todo';
 function TodoList (props) {
           return (
               <div>
                   {
                       props.todo.map((todoObj) => {
-                          console.log(todoObj);
-                          return  (<div key="{todoObj}">{todoObj.task}</div>)
+                          return  (
+                              <Todo key={todoObj.id} todo={todoObj} markComplete={props.markComplete}/>
+                              )
                       })
                   }
               </div>
